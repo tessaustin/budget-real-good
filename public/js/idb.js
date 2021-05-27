6073bb1a9a756a16101d98a6faa851db07919c58
Tess Austin <tessaustin92@gmail.com>
2021-05-26 18:03:58 -0600

    offline file added, in process of testing

--- public/js/idb.js
+++ public/js/idb.js
@@ -21,3 +21,38 @@ request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
 
+function saveRecord(record) {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['pending'], 'readwrite');
+    // access your pending object store
+    const store = transaction.objectStore('pending');
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+                    const transaction = db.transaction(['pending'], 'readwrite');
+                    const store = transaction.objectStore('pending');
+                    // clear all items in your store
+                    store.clear();
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
